a1d46c7bc68a2b6db24d85ca902a113ea23d536a
Obi <obinnawalker@gmail.com>
2017-11-25 11:37:36 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -23,7 +23,7 @@ class Question{
     const self = this
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
-        seld.id = this.lastID
+        self.id = this.lastID
         resolve("Row inserted!")
       })
     })
